13dfff164bed827fb85d24f1b41f90afdca6b1d7
Nick Rudak <rudak@denic.de>
2024-04-02 18:45:49 +0200

    Created script to generate Tsig Secrets

---
+++ gen-secrets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p ./secrets
+
+if [ ! -f "./secrets/secrets.yml"  ]; then
+(
+    echo "---"
+    echo "TsigKey: "
+    echo "WebhookSecret: "
+) > ./secrets/secrets.yml
+fi
+
+TSIGKEY=$(keymgr -t my_name hmac-sha384 | awk '{print $2}' | tail -n 1)
+
+echo $TSIGKEY
+sed "s|^TsigKey:.*\$|TsigKey: $TSIGKEY|" -i ./secrets/secrets.yml
