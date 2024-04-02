4c4d0ba15fa8f95dbab644270ee1550fab281836
Nick Rudak <rudak@denic.de>
2024-04-02 16:32:25 +0200

    Changed script so that it wont run knotc on the first run

--- get-new-zone.sh
+++ get-new-zone.sh
@@ -2,4 +2,6 @@
 
 wget  https://raw.githubusercontent.com/Freestyle00/DNS-Zonen-Remotemanager/main/iot4everyone.de.zone -O ./iot4everyone.de.zone
 
+if [ "$1" != "--first-run" ]; then
 knotc zone-reload
+fi;
