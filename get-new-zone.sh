8118548416fc5b2171d0da90cc78db9f92172f55
Nick Rudak <69159664+Freestyle00@users.noreply.github.com>
2024-05-23 18:03:04 +0200

    Update get-new-zone.sh

--- get-new-zone.sh
+++ get-new-zone.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 logger "starting download"
-wget  https://raw.githubusercontent.com/Freestyle00/DNS-Zonen-Remotemanager/main/iot4everyone.de.zone -O ./iot4everyone.de.zone
+wget --no-cache https://raw.githubusercontent.com/Freestyle00/DNS-Zonen-Remotemanager/main/iot4everyone.de.zone -O ./iot4everyone.de.zone
 logger "finished download"
 
 if [ "$1" != "--first-run" ]; then
